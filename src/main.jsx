9e4f53a441c1db8e9bc228919b18bae4afffee18
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-08-11 01:44:24 +0500

    mobile app setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { ScrollProvider } from "./context/ScrollContext";
 
 ReactDom.createRoot(document.getElementById("root")).render(
-  
-    <ThemeProvider>
-      <ScrollProvider>
-        <App />
-      </ScrollProvider>
-    </ThemeProvider>
+
+  <ThemeProvider>
+    <ScrollProvider>
+      <App />
+    </ScrollProvider>
+  </ThemeProvider>
 )
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
