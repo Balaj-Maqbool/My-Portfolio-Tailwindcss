b8b6ca606744b0cc26852be9dee7609379df2cef
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-08-11 19:58:37 +0500

    back to top animation

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -11,10 +11,10 @@ const BackToTop = () => {
     return (
         <button
             onClick={scrollToTop}
-            className="cosmic-button-secondary border-primary fixed bottom-7  p-1 sm:p-1.5 md:p-2 lg:p-3  right-4 rounded-full z-200 "
+            className="cosmic-button-secondary border-primary fixed bottom-7 animate-[bounce_ease-in-out_6s_infinite] p-1.5 sm:p-2 md:p-2 lg:p-3  xl:p-4 right-4 rounded-full z-200 "
             aria-label= "Back to top"
         >
-        < ArrowUp className='text-accent' />
+        < ArrowUp className= ' text-accent' />
         </button >
     );
 };
