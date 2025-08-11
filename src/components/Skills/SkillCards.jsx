7c3259d46fc402f8e6a518e4ea091983f13bc55b
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-08-12 01:28:02 +0500

    testing 7

--- src/components/Skills/SkillCards.jsx
+++ src/components/Skills/SkillCards.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import useInView from "../../hooks/IntersectionObserver.js";
 
 const SkillCards = ({ skill }) => {
-    const { ref, isVisible } = useInView({ threshold: .1, rootMargin: "-50px 0px 0px 0px", triggerOnce: true });
+    const { ref, isVisible } = useInView({ threshold: .1, rootMargin: "-100px 0px 0px 0px", triggerOnce: true });
     // console.log(ref,isVisible);
     
     return (
-        <div  className={` ${isVisible && skill.animation} rounded-xs z-50  bg-project ring-1 ring-primary/80   p-3 px- lg:p-3.5 sm:p-2.5  w-full  hover:scale-110 `}>
+        <div  className={` ${isVisible ? skill.animation:""} rounded-xs z-50  bg-project ring-1 ring-primary/80   p-3 px- lg:p-3.5 sm:p-2.5  w-full  hover:scale-110 `}>
             <h3 className="text-start text-[.75rem]  sm:text-[.67rem] md:pb-2 md:text-sm ">{skill.name}</h3>
             {
                 <div className="min-w-full mx-auto h-[.25rem] sm:h-1 md:h-1.5 xl-h-2 relative bg-primary/10 origin-top   rounded-full ">
